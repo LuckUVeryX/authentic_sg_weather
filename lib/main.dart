3d67f8b49b6451d268e230d75fb094cc82c8a2ab
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-07-08 19:35:37 +0800

    added screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Authentic SG Weather',
       initialRoute: AppPages.initial,
       getPages: AppPages.routes,
